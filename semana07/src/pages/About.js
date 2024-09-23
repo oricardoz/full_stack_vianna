8723118f2bf84ff0001e9b1f273175167c0073cb
Ricardo Zaghetto <ricardoapzaghetto@gmail.com>
2024-09-22 22:18:33 -0300

    Final commit semana07

---
+++ semana07/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Essa é a página sobre.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ semana07/src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>Essa é a página de contato.</p>
+    </div>
+  );
+}
+
+export default Contact;
